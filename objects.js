c04b3d6bfe9807963d23fa610779051962e88273
Nicholas Stano <nicholasstano@gmail.com>
2019-04-07 15:06:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
 
 
 
